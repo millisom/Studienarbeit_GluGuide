d622b2896215dc9bd04665d130c697acb0aabcdc
millisom <eliza.liebt.sommer@gmail.com>
2026-03-16 13:18:36 +0100

    fix: added Log Out Function

--- gluGuideSetup/server/config/routeGroups/authRoutes.js
+++ gluGuideSetup/server/config/routeGroups/authRoutes.js
@@ -9,6 +9,22 @@ const setupAuthRoutes = (app) => {
   app.use('/', authRoutes);
   app.use('/', profileRoutes);
   
+  // ADD LOGOUT HERE
+  app.post('/logout', (req, res) => {
+    if (req.session) {
+      req.session.destroy((err) => {
+        if (err) {
+          console.error("Error destroying session:", err);
+          return res.status(500).json({ error: "Could not log out" });
+        }
+        res.clearCookie('connect.sid', { path: '/' }); 
+        return res.status(200).json({ message: "Logout successful" });
+      });
+    } else {
+      res.status(200).json({ message: "Already logged out" });
+    }
+  });
+
   // Current user endpoint
   app.get('/currentUser', (req, res) => {
     if (req.session && req.session.userId) {
@@ -18,4 +34,4 @@ const setupAuthRoutes = (app) => {
   });
 };
 
-module.exports = setupAuthRoutes; 
+module.exports = setupAuthRoutes;
