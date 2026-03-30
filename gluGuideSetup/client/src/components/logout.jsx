5a1b584ed170576de7800299bf415ee13817c872
millisom <eliza.liebt.sommer@gmail.com>
2026-03-30 17:11:05 +0200

    update: language logout

--- gluGuideSetup/client/src/components/logout.jsx
+++ gluGuideSetup/client/src/components/logout.jsx
@@ -2,24 +2,28 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from '../styles/BlogCard.module.css';
 import { useAuth } from '../context/AuthContext'; 
+import { useTranslation } from 'react-i18next';
 
 const Logout = () => {
   const navigate = useNavigate();
   const { logout } = useAuth();
+  const { t } = useTranslation();
 
   const handleLogout = async () => {
     try {
       await logout();
-      alert('You have been logged out successfully.');
+      alert(t('logout.success'));
       navigate('/'); 
     } catch (error) {
       console.error('Error logging out:', error);
-      alert('Failed to log out. Please try again.');
+      alert(t('logout.error'));
     }
   };
 
   return (
-    <button className={styles.squareButton} onClick={handleLogout}>Logout</button>
+    <button className={styles.squareButton} onClick={handleLogout}>
+      {t('logout.button')}
+    </button>
   );
 };
 
