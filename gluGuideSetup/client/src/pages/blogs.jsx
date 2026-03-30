91ebd928ada46fc72bbed0f9c68d2a6d2ef5f328
millisom <eliza.liebt.sommer@gmail.com>
2026-03-30 17:39:07 +0200

    update: language Blogs

--- gluGuideSetup/client/src/pages/blogs.jsx
+++ gluGuideSetup/client/src/pages/blogs.jsx
@@ -2,11 +2,14 @@ import CreatePost from "../components/CreatePost";
 import React from "react";
 import styles from '../styles/Blog.module.css';
 import ViewBlogEntries from "../components/ViewBlogEntries";
+import { useTranslation } from "react-i18next";
 
 const Blogs = () => {
+    const { t } = useTranslation();
+
     return (
         <div className={styles.blogs}>
-            <h1 className={styles.pageTitle}>Our Blogs</h1>
+            <h1 className={styles.pageTitle}>{t('blogs.pageTitle')}</h1>
             <div className={styles.section}>
                 <CreatePost />
             </div>
